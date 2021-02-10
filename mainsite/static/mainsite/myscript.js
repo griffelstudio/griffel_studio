09fcb81d7101e7ec4ff1c7bfefe832bfc8035cf7
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-02-10 13:56:03 +0300

    feat: add popup, change main font

--- mainsite/static/mainsite/myscript.js
+++ mainsite/static/mainsite/myscript.js
@@ -8,7 +8,73 @@ const footer = document.querySelector(".footer");
 const formContent = document.querySelector(".connect__formBlockContent");
 const burgerMenu = document.querySelector(".burger");
 const link = document.createElement('div');
-link.text
+const news = document.querySelectorAll(".news__item");
+const newsContainer = document.querySelector(".news__items");
+
+
+for(let i = 0; i < news.length; i++){
+  news[i].addEventListener("click", (event)=>{
+    if (event.target.textContent == 'Read more'){
+      document.body.style.overflowY = "hidden";
+      let fon = document.createElement("div");
+      fon.classList.add("fon");
+      document.body.prepend(fon);
+
+      let popupBody = document.createElement("div");
+      popupBody.classList.add("popup");
+      setTimeout(() => {
+        popupBody.classList.add("popupOpen");
+      }, 50)
+
+      document.body.appendChild(popupBody);
+
+
+      let popupClose = document.createElement("div");
+      popupClose.classList.add("popupClose");
+      popupClose.innerHTML =`<div class="modal-close" for="modal">&#10005;</div>`;
+      popupBody.appendChild(popupClose);
+
+      popupClose.addEventListener("click",()=>{
+        document.body.removeChild(popupBody);
+        document.body.removeChild(fon);
+        document.body.style.overflowY = "scroll";
+        popupBody.classList.remove("popupOpen");
+      })
+
+      let popupContent = document.createElement("div");
+      popupContent.classList.add("popupContent");
+      popupBody.appendChild(popupContent);
+      popupContent.style.overflowY = "scroll";
+
+
+
+
+
+      let popupTitle = document.createElement("div");
+      popupTitle.classList.add("block__title");
+      popupTitle.classList.add("popupTitle");
+      popupTitle.textContent = event.target.previousElementSibling.previousElementSibling.textContent;
+      popupContent.appendChild(popupTitle);
+
+
+      let popupText = document.createElement("div");
+      popupText.classList.add("block__text32");
+      popupText.classList.add("popupText");
+      popupText.textContent = event.target.previousElementSibling.textContent;
+      popupContent.appendChild(popupText);
+
+      fon.addEventListener("click",()=>{
+        document.body.removeChild(popupBody);
+        document.body.removeChild(fon);
+        document.body.style.overflowY = "scroll";
+        popupBody.classList.remove("popupOpen");
+      })
+
+    }
+  })
+}
+
+
 
 function burger(x) {
   x.classList.toggle("change");
@@ -20,7 +86,6 @@ window.addEventListener("scroll", ()=>{
   if(window.pageYOffset > 150){
     header.style.backgroundColor = "rgb(255,255,255,0.95)"
     header.opacity = "0.6"
-    // header.style.filter = "blur(4px)";
     header.style.boxShadow = "0px 4px 30px rgba(47, 47, 47, 0.2)";
   }
   else{
@@ -65,7 +130,6 @@ window.addEventListener("load",()=>{
   }
 })
 function succesSend(){
-  // document.querySelector(".connect__formBlock").style.alignItems = "flex-start";
   formContent.style.alignSelf = "flex-start";
   formContent.style.justifyContent = "center"
   formContent.innerHTML = `
@@ -74,168 +138,3 @@ function succesSend(){
   `;
 
 }
-
-
-// const images = document.querySelectorAll('.img-normal');
-// const unscale = document.querySelectorAll('#img-unscale');
-// const arrow = document.querySelector('.arrowbox');
-// const videoCover = document.querySelector('.videoCover');
-// const player = document.querySelector('.player');
-// const video = document.querySelector("#video");
-// const links = document.querySelectorAll('.activelink');
-// const linkarrow = document.querySelector('#linkArrow');
-// const copyText1 = document.querySelector("#copy-text1");
-// const copyText2 = document.querySelector("#copy-text2");
-// const end = document.querySelector('.end');
-// const fotoBox = document.querySelector(".foto-box");
-// const arrowLeft = document.querySelector(".arrowLeft");
-// const arrowRight = document.querySelector(".arrowRight");
-// const teamBlock = document.querySelector(".team");
-// let activeLink = document.querySelector('.active-link');
-// let dscreen = document.createElement('div');
-//
-// dscreen.classList.add('darkscreen_on');
-//
-// function copyText(copyText) {
-//   let area = document.createElement("textarea");
-//   area.value = copyText.textContent;
-//   document.body.appendChild(area).select();
-//   document.execCommand("copy");
-//   area.remove();
-// }
-//
-// if(screen.availWidth > 767){
-//   if(videoCover){
-//       videoCover.addEventListener('mouseover',function(){
-//         player.style.display = 'block';
-//         document.querySelector('main').appendChild(dscreen);
-//         videoCover.style.display = 'none';
-//         if(dscreen){
-//           dscreen.addEventListener('click',function(){
-//             dscreen.remove();
-//             player.classList.remove('player');
-//             player.classList.add('squeeze');
-//           })
-//           document.addEventListener('keydown', function(event) {
-//             if (event.code == 'Escape'){
-//               dscreen.remove();
-//               player.classList.remove('player');
-//               player.classList.add('squeeze');
-//             }
-//           })
-//         }
-//       })
-//   }
-// }else{
-//   if(videoCover){
-//     videoCover.addEventListener('mouseover',function(){
-//       player.style.display = 'block';
-//       player.classList.add('squeeze');
-//       videoCover.style.display = 'none';
-//     })
-//   }
-// }
-//
-//   let scale = function(image,index){
-//     image.addEventListener('click',function(){
-//       document.querySelector('main').appendChild(dscreen);
-//       images[index].classList.add('scale-img');
-//     })
-// }
-//
-// if(screen.availWidth > 767){
-//   for (var i = 0; i < images.length; i++) {
-//     scale(images[i],i);
-//   }
-// }
-// if(dscreen){
-//   dscreen.addEventListener('click',function(){
-//     dscreen.remove();
-//     for (var i = 0; i < images.length; i++) {
-//       images[i].classList.remove('scale-img');
-//     }
-//   })
-// }
-//
-// if(arrow){
-//   arrow.addEventListener('click',function(){
-//   if (window.pageYOffset > 2000){
-//     window.scroll(0, 0);
-//   }
-//   else{
-//     window.scrollBy(0, window.innerHeight);
-//   }
-//   })
-//   window.onscroll = function () {
-//     if (window.pageYOffset > 2000){
-//       arrow.style.transform = "rotate(180deg)";
-//       arrow.style.transition = '1s';
-//     }
-//     else if (window.pageYOffset == 0) {
-//       arrow.style.transform = "rotate(0deg)";
-//       arrow.style.transition = '1s';
-//     }
-//   }
-// }
-//
-// let moveArrow = function(link){
-//   link.addEventListener('mouseover', function(){
-//     linkarrow.style.left = 1 + 'vw';
-//     linkarrow.style.transition = '0.5s';
-//     linkarrow.style.top = link.getBoundingClientRect().top - 65 + 'px'
-//   })
-//
-//   link.addEventListener('mouseout', function(){
-//       linkarrow.style.left = -10 + '%';
-//       linkarrow.style.transition = '0.5s';
-//   })
-// }
-// for (var i = 0; i < links.length; i++) {
-//   moveArrow(links[i]);
-// }
-// function getnoneActev(){
-//   activeLink.classList.remove('active-link');
-// }
-//
-// const team = [
-//   ['Alexander Mikhasenak','Chief executive officer','/static/mainsite/img/mih.jpg'],
-//   ['Kiryl Ambrazheichyk','Chief technical director','/static/mainsite/img/kirya.jpg'],
-//   ['Alexander Volodin','Chief informational officer','/static/mainsite/img/volod.jpg'],
-//   ['Maryna Karaban','Promotion specialist ','/static/mainsite/img/maryna.jpg'],
-// ]
-// function createTeam(team){
-//   let conteiner;
-//   let foto;
-//   let name;
-//   for(let i = 0; i < 3;i++){
-//     conteiner =  document.createElement('div');
-//     conteiner.classList.add(`foto-conteiner`);
-//     conteiner.textContent = team[i][1]
-//     foto = document.createElement('div');
-//     foto.classList.add(`foto`);
-//     foto.style.backgroundImage = `url(${team[i][2]})`;
-//     foto.style.backgroundSize = `cover`;
-//     foto.style.backgroundPosition = `center`;
-//     name =  document.createElement('div');
-//     name.inrerHTML = `${team[i][0]}`
-//     name.textContent = team[i][0];
-//     conteiner.prepend(name);
-//     conteiner.prepend(foto);
-//     fotoBox.append(conteiner)
-//   }
-// }
-//
-//
-// if(teamBlock){
-//   createTeam(team);
-//   arrowLeft.addEventListener('click',()=>{
-//     team[team.length-1] = team.shift();
-//     fotoBox.innerHTML = '';
-//     createTeam(team)
-//   })
-//   arrowRight.addEventListener('click',()=>{
-//     team.unshift(team.pop())
-//     fotoBox.innerHTML = '';
-//     createTeam(team)
-//   })
-// }
