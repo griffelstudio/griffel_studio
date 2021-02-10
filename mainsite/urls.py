38b6a2924c6964b3495843623cd46ac2f2ad7de4
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-02-10 17:11:32 +0300

    fix:news

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from .views import Index,Team,Products,Contacts,Exporter,Exporter_api,Wmrx,form,Privacy,Privacy_mobile
+from .views import Index,Team,Products,Contacts,Exporter,Exporter_api,Wmrx,form,Privacy,Privacy_mobile,News
 
 urlpatterns = [
     path('', Index.as_view(), name='index_url'),
     path('products/', Products.as_view(), name='products_url'),
-    # path('news/', News.as_view(), name='news_url'),
+    path('news/', News.as_view(), name='news_url'),
     path('team/', Team.as_view(), name='team_url'),
     path('products/exporter/', Exporter.as_view(), name='exporter_url'),
     path('products/exporter_api/', Exporter_api.as_view(), name='exporter_api_url'),
