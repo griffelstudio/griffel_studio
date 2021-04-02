0d1cce9a8866d8cdfe5db0150822c5fe9c4a3b77
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-04-02 09:34:21 +0300

    fix: blog page

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -5,7 +5,7 @@ from .views import Index,Team,Products,Contacts,Exporter,Exporter_api,Wmrx,forms
 urlpatterns = [
     path('', Index.as_view(), name='index_url'),
     path('products/', Products.as_view(), name='products_url'),
-    path('news/', News.as_view(), name='news_url'),
+    path('blog/', News.as_view(), name='news_url'),
     path('team/', Team.as_view(), name='team_url'),
     path('products/exporter/', Exporter.as_view(), name='exporter_url'),
     path('products/exporter_api/', Exporter_api.as_view(), name='exporter_api_url'),
